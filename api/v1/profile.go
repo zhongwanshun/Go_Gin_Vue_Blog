489c4732695af6bb9cb36729aff2e66ed91ece94
zhongwanshun <1572662085@qq.com>
2021-12-22 09:44:31 +0800

    add

---
+++ api/v1/profile.go
@@ -0,0 +1,33 @@
+package v1
+
+import (
+	"ZWS_Go/model"
+	"ZWS_Go/utils/errmsg"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetProfile(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	data, code := model.GetProfile(id)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
+
+func UpdateProfile(c *gin.Context) {
+	var data model.Profile
+	id, _ := strconv.Atoi(c.Param("id"))
+	_ = c.ShouldBindJSON(&data)
+
+	code := model.UpdateProfile(id, &data)
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
+}
--- utils/setting.go
+++ utils/setting.go
@@ -6,41 +6,52 @@ import (
 	"gopkg.in/ini.v1"
 )
 
-//引入变量
 var (
-	AppMode    string
-	HttpPort   string
+	AppMode  string
+	HttpPort string
+	JwtKey   string
+
 	Db         string
 	DbHost     string
 	DbPort     string
 	DbUser     string
 	DbPassWord string
 	DbName     string
+
+	AccessKey  string
+	SecretKey  string
+	Bucket     string
+	QiniuSever string
 )
 
-//一个包初始化的一个函数接口
 func init() {
-	//file获得的是一个结构体,Load返回的是一个file的指针
 	file, err := ini.Load("config/config.ini")
 	if err != nil {
 		fmt.Println("配置文件读取错误，请检查文件路径:", err)
 	}
-	//实现结构体
 	LoadServer(file)
 	LoadData(file)
-
+	LoadQiniu(file)
 }
+
 func LoadServer(file *ini.File) {
-	//读取相关变量(去server中取AppMode,取不到就用默认debug)
 	AppMode = file.Section("server").Key("AppMode").MustString("debug")
-	HttpPort = file.Section("server").Key("HttpPort").MustString("3000")
-
+	HttpPort = file.Section("server").Key("HttpPort").MustString(":3000")
+	JwtKey = file.Section("server").Key("JwtKey").MustString("89js82js72")
 }
+
 func LoadData(file *ini.File) {
 	Db = file.Section("database").Key("Db").MustString("debug")
 	DbHost = file.Section("database").Key("DbHost").MustString("localhost")
 	DbPort = file.Section("database").Key("DbPort").MustString("3306")
-	DbUser = file.Section("database").Key("DbUser").MustString("root")
-	DbPassWord = file.Section("database").Key("DbPassWord").MustString("1234")
+	DbUser = file.Section("database").Key("DbUser").MustString("ginblog")
+	DbPassWord = file.Section("database").Key("DbPassWord").MustString("admin123")
 	DbName = file.Section("database").Key("DbName").MustString("ginblog")
+}
+
+func LoadQiniu(file *ini.File) {
+	AccessKey = file.Section("qiniu").Key("AccessKey").String()
+	SecretKey = file.Section("qiniu").Key("SecretKey").String()
+	Bucket = file.Section("qiniu").Key("Bucket").String()
+	QiniuSever = file.Section("qiniu").Key("QiniuSever").String()
 }
