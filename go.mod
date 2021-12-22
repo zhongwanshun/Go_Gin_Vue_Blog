04d733f3a94f185cc03376be09b716cbd77ea09d
zhongwanshun <1572662085@qq.com>
2021-12-22 09:55:05 +0800

    add

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module ZWS_Go
 go 1.16
 
 require (
+	github.com/gin-contrib/multitemplate v0.0.0-20211002122701-e9e3201b87a0
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-playground/validator/v10 v10.9.0
 	github.com/golang/protobuf v1.5.2 // indirect
@@ -10,10 +11,10 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/qiniu/go-sdk/v7 v7.11.0
 	github.com/ugorji/go v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
-	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/ini.v1 v1.66.2
 	gopkg.in/yaml.v2 v2.4.0 // indirect
---
+++ model/Upload.go
@@ -0,0 +1,43 @@
+package model
+
+import (
+	"ZWS_Go/utils"
+	"ZWS_Go/utils/errmsg"
+	"context"
+	"mime/multipart"
+
+	"github.com/qiniu/go-sdk/v7/auth/qbox"
+	"github.com/qiniu/go-sdk/v7/storage"
+)
+
+var AccessKey = utils.AccessKey
+var SecretKey = utils.SecretKey
+var Bucket = utils.Bucket
+var ImgUrl = utils.QiniuSever
+
+func UpLoadFile(file multipart.File, fileSize int64) (string, int) {
+	putPolicy := storage.PutPolicy{
+		Scope: Bucket,
+	}
+	mac := qbox.NewMac(AccessKey, SecretKey)
+	upToken := putPolicy.UploadToken(mac)
+
+	cfg := storage.Config{
+		Zone:          &storage.ZoneHuadong,
+		UseCdnDomains: false,
+		UseHTTPS:      false,
+	}
+
+	putExtra := storage.PutExtra{}
+
+	formUploader := storage.NewFormUploader(&cfg)
+	ret := storage.PutRet{}
+
+	err := formUploader.PutWithoutKey(context.Background(), &ret, upToken, file, fileSize, &putExtra)
+	if err != nil {
+		return "", errmsg.ERROR
+	}
+	url := ImgUrl + ret.Key
+	return url, errmsg.SUCCSE
+
+}
